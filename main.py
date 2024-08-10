5f8bbcff0619cd32737d90e76f0fb0cd41d3f105
th1nX <Flex0YT@yandex.ru>
2024-08-10 16:38:35 +0300

    Modrith API Library mod search test

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-sas = 0
+import modrinth
+
+project = modrinth.Projects.Search('3D skin').hits[0]
+versions_id = project.versions
+print(versions_id)
+for id in versions_id:
+    version = project.getVersion(id)
+    primaryFile = version.getPrimaryFile()
+    print(version.getDownload(primaryFile))
