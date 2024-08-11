1900d20ffbef29d784a0f99f1a159801ae511c92
yorw <vbberchenko@gmail.com>
2024-08-11 17:26:27 +0300

    mod downloader alpha test

--- mod_donloader.py
+++ mod_donloader.py
@@ -8,4 +8,4 @@ def get_mod(name, version, loader):
     mod_download = pyrinth.Project.get(mod_id).get_latest_version(loaders=[loader], game_versions=[version])
     print(mod_download)
 
-get_mod("Mod menu", "1.19.4", "fabric")
+get_mod("Mod menu", "1.19.4", "fabric" )
