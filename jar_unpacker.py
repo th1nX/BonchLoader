bc48ed73035986fa3a44da178be2b786c142599e
yorw <vbberchenko@gmail.com>
2024-08-11 19:18:57 +0300

    Jar unpacker update 0.2

--- jar_unpacker.py
+++ jar_unpacker.py
@@ -1,13 +1,40 @@
 import zipfile
 import os
 import json
+from fnmatch import fnmatch
 
-path = "./test_mods"
+path = r"./test_mods"
+
+mods_names = []
+e_mods = []
+e_count = 0
 
 for filename in os.listdir(path):
-    mod = zipfile.ZipFile(f'{path}/{filename}', 'r')
-    modinfo = mod.read("fabric.mod.json")
-    modinfo_str = modinfo.decode("utf-8")
-    modinfo_json = json.loads(modinfo_str)
-    print(modinfo_json["name"])
-    print(modinfo_json["depends"])
+    if fnmatch(filename, '*.jar'):
+        mod = zipfile.ZipFile(f'{path}/{filename}', 'r')
+        try:
+            modinfo = mod.read("fabric.mod.json")
+            modinfo_str = modinfo.decode("utf-8")
+            modinfo_json = json.loads(modinfo_str)
+            modinfo_name = modinfo_json["name"]
+            mods_names.append(modinfo_name)
+        except:
+            try:
+                modinfo = mod.read("META-INF/mods.toml")
+                modinfo_str = modinfo.decode("utf-8")
+                modinfo_split = modinfo_str.split("\n")
+
+                for line in modinfo_split:
+                    if line.count("displayName"):
+                        name_line = line
+
+                name_line_replace = name_line.replace("displayName=", "").replace('"', "")
+                modinfo_name = name_line_replace.replace("displayName", "").replace("=", "").strip()
+                mods_names.append(modinfo_name)
+            except:
+                e_count += 1
+                e_mods.append(mod)
+
+print("Mods names:",mods_names)
+print("Errors:", e_count)
+print("Error mods:", e_mods)
