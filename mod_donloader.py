3e4eb1431c74a0693b096c53f938d805e575d932
yorw <vbberchenko@gmail.com>
2024-08-11 17:24:35 +0300

    add jar_unpacker alpha test

---
+++ mod_donloader.py
@@ -0,0 +1,11 @@
+import modrinth
+import pyrinth
+
+
+def get_mod(name, version, loader):
+    mod_search = pyrinth.Project.search(name, limit=1)
+    mod_id = mod_search[0].model.project_id
+    mod_download = pyrinth.Project.get(mod_id).get_latest_version(loaders=[loader], game_versions=[version])
+    print(mod_download)
+
+get_mod("Mod menu", "1.19.4", "fabric")
