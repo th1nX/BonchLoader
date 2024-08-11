1b057a2848f3dad12da9e7f8d04de1abdf425123
yorw <vbberchenko@gmail.com>
2024-08-11 19:51:04 +0300

    Jar unpacker update 0.3

--- jar_unpacker.py
+++ jar_unpacker.py
@@ -3,38 +3,45 @@ import os
 import json
 from fnmatch import fnmatch
 
-path = r"./test_mods"
+path = r"C:\Users\vbber\AppData\Roaming\PolyMC\instances\Tech&Magic\.minecraft\mods"
 
-mods_names = []
-e_mods = []
-e_count = 0
-
-for filename in os.listdir(path):
-    if fnmatch(filename, '*.jar'):
-        mod = zipfile.ZipFile(f'{path}/{filename}', 'r')
-        try:
-            modinfo = mod.read("fabric.mod.json")
-            modinfo_str = modinfo.decode("utf-8")
-            modinfo_json = json.loads(modinfo_str)
-            modinfo_name = modinfo_json["name"]
-            mods_names.append(modinfo_name)
-        except:
+def jar_unpack(path):
+    mods_names = []
+    e_mods = []
+    e_count = 0
+    for filename in os.listdir(path):
+        if fnmatch(filename, '*.jar'):
+            mod = zipfile.ZipFile(f'{path}/{filename}', 'r')
             try:
-                modinfo = mod.read("META-INF/mods.toml")
+                modinfo = mod.read("fabric.mod.json")
                 modinfo_str = modinfo.decode("utf-8")
-                modinfo_split = modinfo_str.split("\n")
-
-                for line in modinfo_split:
-                    if line.count("displayName"):
-                        name_line = line
-
-                name_line_replace = name_line.replace("displayName=", "").replace('"', "")
-                modinfo_name = name_line_replace.replace("displayName", "").replace("=", "").strip()
+                modinfo_json = json.loads(modinfo_str)
+                modinfo_name = modinfo_json["name"]
                 mods_names.append(modinfo_name)
             except:
-                e_count += 1
-                e_mods.append(mod)
+                try:
+                    modinfo = mod.read("META-INF/mods.toml")
+                    modinfo_str = modinfo.decode("utf-8")
+                    modinfo_split = modinfo_str.split("\n")
 
-print("Mods names:",mods_names)
-print("Errors:", e_count)
-print("Error mods:", e_mods)
+
+                    for line in modinfo_split:
+                        if line.count("displayName"):
+                            name_line = line
+
+                    name_line_replace = name_line.replace("displayName=", "").replace('"', "")
+                    modinfo_name = name_line_replace.replace("displayName", "").replace("=", "").strip()
+                    modinfo_name = modinfo_name.replace("#mandatory", "").strip()
+                    mods_names.append(modinfo_name)
+                except:
+                    e_count += 1
+                    e_mods.append(filename)
+
+    #Потом убрать
+    print("Mods names:",mods_names)
+    print("Errors:", e_count)
+    print("Error mods:", e_mods)
+
+    return mods_names
+
+jar_unpack(path)
