533c058400c7a262451f5d48587203088759c0f3
yorw <vbberchenko@gmail.com>
2024-08-19 16:50:44 +0300

    New UI test

--- main.py
+++ main.py
@@ -13,31 +13,36 @@ def main():
             super().__init__()
 
             self.title("BONCH - Minecraft mod updater")
-            self.geometry("350x190")
+            self.geometry("350x390")
             self.grid_columnconfigure(0, weight=1)
             self.grid_rowconfigure((0, 1), weight=1)
 
             self.button_frame = customtkinter.CTkFrame(self)
-            self.button_frame.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="nsw")
+            self.button_frame.grid(row=1, column=0, padx=10, pady=10, sticky="nw")
             self.title = customtkinter.CTkLabel(self.button_frame, text="Folders", fg_color="gray30", corner_radius=6)
             self.title.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="ew")
             self.button_1 = customtkinter.CTkButton(self.button_frame, text="Mods folder", command=self.select_mod_folder)
             self.button_1.grid(row=1, column=0, padx=10, pady=(10, 0), sticky="w")
             self.button_2 = customtkinter.CTkButton(self.button_frame, text="Download folder", command=self.select_download_folder)
-            self.button_2.grid(row=2, column=0, padx=10, pady=(10, 0), sticky="w")
+            self.button_2.grid(row=2, column=0, padx=10, pady=10, sticky="w")
 
             self.button_frame2 = customtkinter.CTkFrame(self)
-            self.button_frame2.grid(row=0, column=1, padx=(0, 10), pady=(10, 0), sticky="nsw")
+            self.button_frame2.grid(row=1, column=0, padx=(180, 0), pady=10, sticky="nw")
             self.title = customtkinter.CTkLabel(self.button_frame2, text="Version", fg_color="gray30", corner_radius=6)
             self.title.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="ew")
 
             self.entry = customtkinter.CTkEntry(self.button_frame2, placeholder_text="Minecraft version")
             self.entry.grid(row=1, column=0, padx=10, pady=(10, 0), sticky="w")
             self.combobox = customtkinter.CTkComboBox(self.button_frame2, values=["Fabric", "Forge", "NeoForge", "Quilt"])
-            self.combobox.grid(row=2, column=0, padx=10, pady=(10, 0), sticky="w")
+            self.combobox.grid(row=2, column=0, padx=10, pady=10, sticky="w")
+
+            self.textbox = customtkinter.CTkTextbox(self)
+            self.textbox.grid(row=0, column=0, sticky="we", padx=10, pady=(10, 0))
 
             self.button = customtkinter.CTkButton(self, text="Start update", command=self.start_update)
-            self.button.grid(row=3, column=0, padx=10, pady=10, sticky="ew", columnspan=2)
+            self.button.grid(row=2, column=0, padx=20, pady=(0, 10), sticky="sew", columnspan=1)
+
+            self.textbox.insert(0.0, f"\nBONCH - Best minecraft modpack updater\n")
 
         def start_update(self):
             mainDirectory = os.getcwd()
@@ -54,11 +59,12 @@ def main():
             global mod_path
             mod_path = filedialog.askdirectory()
             print('choosed mod path:', mod_path)
-
+            self.textbox.insert(0.0, "\n>> " +f"Mod path: {mod_path}" + "\n")
         def select_download_folder(self):
             global download_path
             download_path = filedialog.askdirectory()
             print('choosed download path:', download_path)
+            self.textbox.insert(0.0, "\n>> " +f"Download path: {mod_path}" + "\n")
 
     app = App()
     app.resizable(False, False)
